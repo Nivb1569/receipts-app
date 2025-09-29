1736b3519838374bfec20be25fffeb86e29d5bc4
Niv Baumel <nivbaumel2@gmail.com>
2025-09-29 18:25:24 +0300

    feat: update package.json with new Prisma commands and Docker scripts; add seed.ts file for database seeding refactor: clean up schema.prisma formatting and remove unnecessary comments

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -1,34 +1,26 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+// eslint.config.mjs
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
 
-export default tseslint.config(
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
   {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
+    plugins: { prettier },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
+      'prettier/prettier': 'off'
+    }
   },
-);
+
+  {
+    files: ['prisma/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off'
+    }
+  }
+]
---
+++ api/prisma/seed.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function seed() {
+  await prisma.customers.createMany({
+    data: [
+      { name: "Niv Baumel", address: "Herzel 5, Tel Aviv" },
+      { name: "John Doe", address: "123 Main St, Springfield" },
+      { name: "Jane Smith", address: "456 Elm St, Metropolis" },
+    ],
+    });
+}
